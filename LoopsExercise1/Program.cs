3827f5ad425a8fefa3bc5c8194c1d4bcc0bc8cca
nwhang1 <nicholasjwhang@gmail.com>
2024-03-26 12:14:46 -0700

    15 loops manually entered

---
+++ LoopsExercise1/Program.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Practice
+{
+    public static void Main(string[] args)
+    {
+        // 1st
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //2nd
+        for (int i = 20; i > 10; i--)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //3rd
+        for (int i = 100; i > 10; i--)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //4th
+        for (int i = 0; i > -10; i--)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //5th
+        for (int i = 0; i <50; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //6th
+        for (int i = 0; i > 5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //7th
+        for(int i = 10; i > 0; i--)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //8th
+        for (int i = 20; i > 10; i--)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //9th
+        for (int i = 200; i > 100; i--)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //10th
+        for (int i = 100; i <200; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //11th
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //12th
+        for (int i = 0; i <3; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //13th
+        for (int i = 10; i < 15;  i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //14th
+        for (int i = 20; i <25; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        //15th
+        for (int i = 0; i <5; i++)
+        {
+            Console.WriteLine($"{i}");
+        }
+
+        Console.WriteLine("Now my wrists hurt.");
+    }
+}
+
